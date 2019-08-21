7e08b5118bb5d446ebc1d4ca04ba02a88c77cbd9
Julie Erlemeier <jmerlemeier@gmail.com>
2019-08-21 11:58:16 -0700

    Eventbrite data successfully rendered to page.

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ function Location(query, format, lat, lng) {
   this.longitude = lng;
 }
 
-// ======= TARGET LOCATION in JSON FILE =======
+// =========== TARGET LOCATION from API ===========
+
 app.get('/location', (request, response) => {
   const query = request.query.data; //request.query is part of the request (NewJohn's hand) and is a vector for questions. It lives in the URL, public info. Postal service of internet.
 
@@ -46,23 +47,24 @@ app.get('/location', (request, response) => {
   })
 })
 
-// ======= TARGET WEATHER in JSON FILE =======
+// =========== TARGET WEATHER from API ===========
+
 app.get('/weather', getWeather)
 
 function getWeather(request, response){
-  console.log(request);
+  // console.log(request);
 
   const localData = request.query.data;
 
   const urlDarkSky = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${localData.latitude},${localData.longitude}`;
 
   superagent.get(urlDarkSky).then(responseFromSuper => {
-    
+
     const weatherData = responseFromSuper.body;
-    console.log('weather', weatherData);
+    // console.log('weather', weatherData);
 
     const eightDays = weatherData.daily.data;
-    
+
     const formattedDays = eightDays.map(day => new Day(day.summary, day.time)
     );
     response.send(formattedDays)
@@ -73,6 +75,35 @@ function getWeather(request, response){
   function Day (summary, time) {
     this.forecast = summary;
     this.time = new Date(time *1000).toDateString();
+  }
+}
+// ============ EVENTBRITE from API ==============
+
+app.get('/events', getEvents)
+
+function getEvents(request, response){
+
+  let eventData = request.query.data;
+
+  const urlfromEventbrite = `https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.latitude=${eventData.latitude}&location.longitude=${eventData.longitude}&token=${process.env.EVENTBRITE_API_KEY}`;
+
+  superagent.get(urlfromEventbrite).then(responseFromSuper => {
+    console.log(responseFromSuper.body)
+
+    const eventbriteData = responseFromSuper.body.events;
+
+    const formattedEvents = eventbriteData.map(event => new Event(event.url, event.name.text, event.start.local, event.description.text));
+
+    response.send(formattedEvents);
+  }).catch(error => {
+    response.status(500).send(error.message);
+    console.error(error);
+  })
+  function Event (link, name, event_date, summary){
+    this.link = link;
+    this.name = name;
+    this.event_date = new Date(event_date).toDateString();
+    this.summary = summary;
   }
 }
 
